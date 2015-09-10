9dd4b9c77c83b09561d8cd29e501cbe54f4d9781
JavierOrdMartin <0619020114@alu.uma.es>
2015-09-10 17:36:27 +0200

    pieza json->alumno

--- ClienteScratch/web/libs/main.js
+++ ClienteScratch/web/libs/main.js
@@ -4,21 +4,21 @@
  * and open the template in the editor.
  */
 
-$(document).ready(function(){
+$(document).ready(function() {
     var counts = [0];
-    var json;
+
     $(".dragIn").draggable({
-        helper:'clone',
-        start: function(){ 
+        helper: 'clone',
+        start: function() {
             counts[0]++;
         }
     });
 
     $(".dragOut").draggable({
-        helper:'original',
+        helper: 'original',
         containment: 'parent'
     });
-    
+
     $("#content-panel").droppable({
         accept: ".dragIn, .dragOut",
         drop: function(ev, ui) {
@@ -31,36 +31,73 @@ $(document).ready(function(){
             }
         }
     });
-     
+
     $("#bin-panel").droppable({
         accept: ".dragOut",
         drop: function(ev, ui) {
             $(ui.draggable).remove();
         }
     });
-    
+
     $("#sortable").sortable();
-    
-    
+
     $("#finalizar").click(
-        function (ev, ui){
-           var items=$("#sortable").find('.piece').map(function(){
-               
-              
-               return item;
-                   
-            
-               
-              
-                 
-              
-              
-            
-           });
-           json=JSON.stringify(items);
-            console.log(items);
-       }
-    );
-    
-   
+            function(ev, ui) {
+                
+                var initElement = $("#sortable");
+                
+                json = mapDOM(initElement, true);
+                console.log(json);
+
+            });
+
+
+    function mapDOM(element, json) {
+        var treeObject = {};
+        
+        treeHTML(element, treeObject);
+
+        return (json) ? JSON.stringify(treeObject) : treeObject;
+    }
+
+    function treeHTML(element, object) {
+        object["type"] = element.nodeName;
+
+        
+        var list = $("#sortable").find(".piece ");
+        for ( var i = 0 , len = list.length; i < len; i++){ 
+           
+            var piece = list[i];
+            console.log(list[i]);
+        
+        
+        }
+
+        /*
+         if (list != null) {
+         
+         if (list.length) {
+         object["content"] = [];
+         for (var i = 0; i < list.length; i++) {
+         if (list[i].nodeType == 3) {
+         object["content"].push(list[i].nodeValue);
+         } else {
+         object["content"].push({});
+         treeHTML(list[i], object["content"][object["content"].length - 1]);
+         }
+         }
+         }
+         }
+         if (element.attributes != null) {
+         if (element.attributes.length) {
+         object["attributes"] = {};
+         for (var i = 0; i < element.attributes.length; i++) {
+         object["attributes"][element.attributes[i].nodeName] = element.attributes[i].nodeValue;
+         }
+         }
+         }
+         */
+        //console.log(object);
+    }
+
 });
